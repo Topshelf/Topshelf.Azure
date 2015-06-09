1b023218d2b86181c046e84f35684b37d74adbd1
Chris Patterson <chris@phatboyg.com>
2015-06-09 14:52:30 -0700

    Gemfile for building

---
+++ Gemfile
@@ -0,0 +1,6 @@
+#ruby=1.9.3
+#gemset=topshelf
+source 'http://rubygems.org'
+gem 'albacore', '~> 0.3.6'
+gem 'rake'
+gem 'semver2'
--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Topshelf.Azure support running Topshelf services as a worker role on Microsoft Azure.")]
 [assembly: AssemblyProduct("Topshelf.Azure")]
 [assembly: AssemblyCopyright("Copyright 2015-2015 Chris Patterson, All rights reserved.")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 
-[assembly: AssemblyInformationalVersion("1.0.0.ba01f0")]
+[assembly: AssemblyInformationalVersion("1.2.0.efd29d")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
